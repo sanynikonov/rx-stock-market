f0ec08e51a9d5c6f9eb65140f57e78f6ba9cf535
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2022-01-08 13:39:45 +0200

    Add client sample

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,23 +1,62 @@
 ﻿using System.Reactive.Linq;
 using System.Text.Json;
+using Api;
 using Business;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-EventHandler MyEventHandler2 = null;
+await RxGrpcTest();
 
-/*var httpClient = new HttpClient();
-var tradeClient = new TradeClient(httpClient);
-
-using var _ = tradeClient.GetTrades().Subscribe(trade =>
+async Task RxGrpcTest()
 {
-    Console.WriteLine(JsonSerializer.Serialize(trade));
-}, e => Console.WriteLine(e.Message));
+    var channel = GrpcChannel.ForAddress("https://localhost:7116", new GrpcChannelOptions
+    {
+        HttpHandler = GetHttpClientHandler()
+    });
 
-Task.Delay(TimeSpan.FromMinutes(2)).Wait();*/
+    var client = new Greeter.GreeterClient(channel);
 
-Observable.Interval(TimeSpan.FromSeconds(1))
-    .Subscribe(time => MyEventHandler2?.Invoke(null, EventArgs.Empty));
+    var call = client.SayHello(new HelloRequest());
+    await foreach (var data in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(data.Message);
+    }
+}
 
-Observable.FromEventPattern(h => MyEventHandler2 += h, h => MyEventHandler2 -= h)
-    .Subscribe(pattern => Console.WriteLine());
+HttpClientHandler GetHttpClientHandler()
+{
+    var httpHandler = new HttpClientHandler();
+    // Return `true` to allow certificates that are untrusted/invalid
+    httpHandler.ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+    return httpHandler;
+}
 
-Task.Delay(50000).Wait();
+void TestRx()
+{
+    /*var httpClient = new HttpClient();
+    var tradeClient = new TradeClient(httpClient);
+
+    using var _ = tradeClient.GetTrades().Subscribe(trade =>
+    {
+        Console.WriteLine(JsonSerializer.Serialize(trade));
+    }, e => Console.WriteLine(e.Message));
+
+    Task.Delay(TimeSpan.FromMinutes(2)).Wait();*/
+
+    EventHandler MyEventHandler2 = null;
+
+    Observable.Interval(TimeSpan.FromSeconds(1))
+        .Subscribe(time => MyEventHandler2?.Invoke(null, EventArgs.Empty));
+
+    Observable.FromEventPattern(h => MyEventHandler2 += h, h => MyEventHandler2 -= h)
+        .Subscribe(pattern => Console.WriteLine());
+
+    Observable.Range(0, 1)
+        .Take(1)
+        .Subscribe();
+
+    Task.Delay(50000).Wait();
+}
