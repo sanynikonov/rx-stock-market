9759c83a59df79ecbce59a5ce4f68b80753705c1
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2022-01-08 13:39:24 +0200

    Create api project with sample grpc stream

---
+++ Api/Services/GreeterService.cs
@@ -0,0 +1,45 @@
+using System.Reactive;
+using System.Reactive.Linq;
+using Api;
+using Grpc.Core;
+
+namespace Api.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task SayHello(HelloRequest request, IServerStreamWriter<HelloReply> streamWriter, ServerCallContext context)
+        {
+            _logger.LogInformation("Opened stream");
+            
+            var tcs = new TaskCompletionSource();
+            context.CancellationToken.Register(() => tcs.SetResult());
+
+            var observable = GetObservable(); //should be service call
+
+            using var _ = observable
+                .SelectMany(async data =>
+                {
+                    await streamWriter.WriteAsync(new HelloReply {Message = "Hello " + data});
+                    return Unit.Default;
+                })
+                .Subscribe();
+
+            await tcs.Task;
+
+            _logger.LogInformation("Closed stream");
+        }
+
+        private static IObservable<string> GetObservable()
+        {
+            return Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Select(i => i.ToString());
+        }
+    }
+}
