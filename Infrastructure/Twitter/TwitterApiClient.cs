ce5e767ce9beb0f590a3d3463b939c25657ee0dd
P0linux <pollykr2000@gmail.com>
2022-01-11 04:06:02 +0200

    Add time range for tweets

--- Infrastructure/Twitter/TwitterApiClient.cs
+++ Infrastructure/Twitter/TwitterApiClient.cs
@@ -5,6 +5,7 @@ using Tweetinvi;
 using Tweetinvi.Core.Extensions;
 using Tweetinvi.Models;
 using Tweetinvi.Models.V2;
+using Tweetinvi.Parameters.V2;
 
 namespace Infrastructure.Twitter;
 
@@ -13,6 +14,13 @@ public class TwitterApiClient : ITwitterApiClient
     private readonly TwitterApiClientSettings _settings;
     private readonly TwitterClient _twitterClient;
 
+    //Test values
+    public string Key = "bUZJDz82U2xTSqMKgdKK1AInk";
+    public string Secret = "X17XzTRsDMA3dQUMPM813SZTwUhwJhcAvXReyoR9Rz2ImS3J95";
+
+    public string Token =
+        "AAAAAAAAAAAAAAAAAAAAAM2XXwEAAAAAtdcdm42KluIcJkHCXIIPhJoq4RU%3DgLXipp5h9hSu3mftr7v12id7rqkIW0mjb5tIZm4GP653pfxvJk";
+    
     public TwitterApiClient(IOptions<TwitterApiClientSettings> settings)
     {
         _settings = settings.Value;
@@ -24,14 +32,29 @@ public class TwitterApiClient : ITwitterApiClient
         
         _twitterClient = new TwitterClient(credentials);
     }
+    
+    public TwitterApiClient()
+    {
+        var credentials = new ConsumerOnlyCredentials(Key, Secret)
+        {
+            BearerToken = Token
+        };
+        
+        _twitterClient = new TwitterClient(credentials);
+    }
 
     public IObservable<TweetV2> GetTweets(CompanyModel companyModel)
     {
         string request = BuildTweetsRequest(companyModel);
         
+        var parameters = new SearchTweetsV2Parameters(request)
+        {
+            StartTime = DateTime.UtcNow.AddMinutes(-5)
+        };
+        
         return Observable.FromAsync(async () =>
         {
-            var search = await _twitterClient.SearchV2.SearchTweetsAsync(request);
+            var search = await _twitterClient.SearchV2.SearchTweetsAsync(parameters);
             return search.Tweets;
         }).SelectMany(x => x);
     }
